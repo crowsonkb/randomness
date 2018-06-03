1039824a687b168e9a8d7e08824a343038792c4a
Katherine Crowson <crowsonkb@gmail.com>
2018-06-03 13:03:37 -0700

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+from pathlib import Path
+
 from setuptools import setup
+
+
+BASEDIR = Path(__file__).resolve().parent
+
 
 setup(name='randomness',
       version='0.1',
       description='Generates random secrets (passwords, etc).',
+      long_description=((BASEDIR / 'README.rst').read_text()),
       url='http://github.com/crowsonkb/randomness',
       author='Katherine Crowson',
       author_email='crowsonkb@gmail.com',
